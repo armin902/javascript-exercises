5a5a4fb3f87424362f8cc65222743274d3a2d4b4
doctorrayjay117 <arminhappy2@gmail.com>
2026-07-17 10:53:59 -0400

    Completed exercise 9, utilized a for-loop and conditionals to check for special cases

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(startIndex, endIndex) {
+    if (!(Number.isInteger(startIndex)) || !(Number.isInteger(endIndex))) {
+        return "ERROR";
+    }
+    if (startIndex < 0 || endIndex < 0) {
+        return "ERROR";
+    }
 
+    let finalSum = 0;
+    for (let i = startIndex; i <= endIndex; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
