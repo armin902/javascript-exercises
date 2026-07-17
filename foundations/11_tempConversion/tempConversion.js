64fabe2aa90cd30a34645fe47fc72801a51e4179
doctorrayjay117 <arminhappy2@gmail.com>
2026-07-17 11:26:15 -0400

    Completed exercise 11, utilized the fahrenheit and celsius conversion formulas, while also using the Math.round() method to correctly round the output

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitInput) {
+  let celsiusConversion = (fahrenheitInput - 32) * (5/9);
+  return Math.round(celsiusConversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusInput) {
+  let fahrenheitConversion = (celsiusInput * (9/5) + 32);
+  return Math.round(fahrenheitConversion * 10) / 10;
 };
 
 // Do not edit below this line
