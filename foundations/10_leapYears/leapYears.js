6c13613dbf3ac7c6324eb0445ffd336b76df5d4b
doctorrayjay117 <arminhappy2@gmail.com>
2026-07-17 11:18:21 -0400

    Completed exercise 10, utilized modulus and conditional logic to determine leap years

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
+    let divBy4 = inputYear % 4;
+    let divBy100 = inputYear % 100;
+    let divBy400 = inputYear % 400;
 
+    if ((divBy4 === 0 && !(divBy100 === 0)) || (divBy100 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
